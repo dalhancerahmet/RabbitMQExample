a2f2801a921205479b05bd7dbe3b0086ecdeb191
ahmet dalhançer <ahmed44dalhancer@hotmail.com>
2022-10-19 14:52:35 +0300

    Add project files.

---
+++ RabbitMQ Example/Program.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.Uri = new Uri("amqps://wfnfeekq:N7I76Tsz7TViCmlw2HocY5v1F0ZrJSQl@cow.rmq2.cloudamqp.com/wfnfeekq");
+        //factory.HostName = "localhost";
+
+        using (IConnection connection = factory.CreateConnection())
+        using (IModel channel = connection.CreateModel())
+        {
+            channel.QueueDeclare("mesajkuyrugu", false, false, true);
+            //queue: Oluşturulacak kuyruğun adını belirliyoruz.
+            //durable: Normal şartlarda kuyruktaki mesajların hepsi bellek üzerinde dizilirler. Hal böyleyken RabbitMQ sunucuları bir sebepten dolayı restart atarlarsa tüm veriler kaybolabilir. durable parametresine true değerini verirsek eğer verilerimiz güvenli bir şekilde sağlamlaştırılacak yani fiziksel hale getirilecektir.
+            //exclusive: Oluşturulacak bu kuyruğa birden fazla kanalın bağlanıp, bağlanmayacağını belirtir.
+            //autoDelete: True değerine karşılık tüm mesajlar bitince kuyruğu otomatik imha eder.
+            byte[] message = Encoding.UTF8.GetBytes("Bu bir rabbitMQ üzerinden gönderilen mesajdır.");
+            channel.BasicPublish(exchange: "", routingKey: "mesajkuyrugu", body: message);
+            //exchange: Eğer exchange kullanmıyorsanız boş bırakınız.Böylece default exchange devreye girecek ve kullanılmış olacaktır.
+            //routingKey: Eğer ki default exchange kullanıyorsanız routingKey olarak oluşturduğunuz kuyruğa verdiğiniz ismin birebir aynısını veriniz.
+            //body : Gönderilecek mesajın ta kendisidir.
+        }
+    }
+}
